adc08cb7993c7ae55b42c7327a846b26a4911cda
Antoine Haddad <89608252+YourAntidote@users.noreply.github.com>
2022-10-14 12:36:58 -0400

    Add files via upload
    3.1 achevé

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let chaptersObj = {
   depart: {
     subtitle: "Sortie du magasin",
     text: "Vous sortez d'un petit magasin et vous marchez vers votre voiture non loin. Cependant, en tournant la tête, vous voyez un homme avec une cagoule qui essaie d'arracher la saccoche d'une dame.",
-    img: "../assets/Images/vole_saccoche.png",
+    img: "./assets/Images/vole_saccoche.png",
     options: [
       {
         text: "Intervenir",
@@ -18,7 +18,7 @@ let chaptersObj = {
   intervenir: {
     subtitle: "Intervenir",
     text: "Votre cri à distrait le voleur, ainsi cela à permis à la dame de s'enfuir. Par contre le voleur n'a pas aimé votre petit jeu et marche dans votre direction.",
-    img: "../assets/Images/pointe_doigt.png",
+    img: "./assets/Images/pointe_doigt.png",
     options: [
       {
         text: "Oh oh..",
@@ -29,7 +29,7 @@ let chaptersObj = {
   menace: {
     subtitle: "Menace",
     text: "À présent que vous avez tout son attention, le voleur sors son fusil et vous pointe avec en vous menacant de vous tuez. Que feriez-vous?",
-    img: "../assets/Images/pointe_fusil.png",
+    img: "./assets/Images/pointe_fusil.png",
     options: [
       {
         text: "Partir à courir",
@@ -40,7 +40,7 @@ let chaptersObj = {
         action: "goToChapter('distration')",
       },
       {
-        text: "Vous faites à semblant que vous le reconnaissez",
+        text: "Faire à semblant de le reconnaître",
         action: "goToChapter('confus')",
       },
     ],
@@ -48,7 +48,7 @@ let chaptersObj = {
   accident: {
     subtitle: "Mort - L'accident",
     text: "Un conducteur imprudent arrive trop vite dans votre direction et n'a pas réagit assez vite pour s'arrêter et il vous rentre très vite dessus. Vous êtes mort",
-    img: "../assets/Images/accident.png",
+    img: "./assets/Images/accident.png",
     options: [
       {
         text: "Recommencer",
@@ -59,7 +59,7 @@ let chaptersObj = {
   distration: {
     subtitle: "Distration",
     text: "Vous avez réussi à le distraire et vous protifez de l'occasion pour y sauter dessus.",
-    img: "../assets/Images/attaque.jpg",
+    img: "./assets/Images/attaque.jpg",
     options: [
       {
         text: "À l'attaaaaque!",
@@ -70,7 +70,7 @@ let chaptersObj = {
   distrationxxx: {
     subtitle: "Distration",
     text: "Vous avez réussi à le distraire et vous protifez de l'occasion pour y sauter dessus.",
-    img: "../assets/Images/attaque.jpg",
+    img: "./assets/Images/attaque.jpg",
     options: [
       {
         text: "À l'attaaaaque!",
@@ -81,7 +81,7 @@ let chaptersObj = {
   tomber: {
     subtitle: "Ennemi tomber à terre",
     text: "En tombant à terre, le voleur échappé son fusil à côté de lui, et il s'empresse de se relevé afin d'aller le récupérer.",
-    img: "../assets/Images/tomber.png",
+    img: "./assets/Images/tomber.png",
     options: [
       {
         text: "Le maintenir à terre",
@@ -96,7 +96,7 @@ let chaptersObj = {
   tomberxxx: {
     subtitle: "Ennemi tomber à terre",
     text: "En tombant à terre, le voleur échappé son fusil à côté de lui, et il s'empresse de se relevé afin d'aller le récupérer.",
-    img: "../assets/Images/tomber.png",
+    img: "./assets/Images/tomber.png",
     options: [
       {
         text: "Le maintenir à terre",
@@ -111,18 +111,18 @@ let chaptersObj = {
   arreter: {
     subtitle: "Victoire - Arrêter",
     text: "Les autorités sont arrivées juste à temps. Le cambrioleur est arrêté et vous êtes sain et sauf.",
-    img: "../assets/Images/arreter.png",
+    img: "./assets/Images/arreter.png",
     options: [
       {
         text: "Recommencer",
-        action: "goToChapter('debut')",
+        action: "goToChapter('depart')",
       },
     ],
   },
   pas_intervenir: {
     subtitle: "Dans la voiture",
     text: "Le braqueur sait que vous l'avez vu, et à présent il vous menace avec son fusil de descendre de votre véhicule et de laisser les clés dans le contacte.",
-    img: "../assets/Images/pointe_fusil.png",
+    img: "./assets/Images/pointe_fusil.png",
     options: [
       {
         text: "Oh oh...",
@@ -133,7 +133,7 @@ let chaptersObj = {
   couteau: {
     subtitle: "Se défendre?",
     text: "Vous avez un de chasse dans votre coffre à gant.",
-    img: "../assets/Images/couteau_voiture.png",
+    img: "./assets/Images/couteau_voiture.png",
     options: [
       {
         text: "Le prendre",
@@ -148,14 +148,14 @@ let chaptersObj = {
   question: {
     subtitle: "Question",
     text: "Vous descendez du véhicule comme il vous l'a demandé. Il vous demande si vous êtes armé.",
-    img: "../assets/Images/pointe_doigt.png",
+    img: "./assets/Images/pointe_doigt.png",
     options: [
       {
         text: "Dire la vérité",
         action: "goToChapter('carJacking')",
       },
       {
-        text: "Vous faîtes à semblant de parler une autre langue",
+        text: "Essayer de parler une autre langue",
         action: "goToChapter('confus')",
       },
     ],
@@ -163,14 +163,14 @@ let chaptersObj = {
   questionxxx: {
     subtitle: "Question",
     text: "Vous descendez du véhicule comme il vous l'a demandé. Il vous demande si vous êtes armé.",
-    img: "../assets/Images/pointe_doigt.png",
+    img: "./assets/Images/pointe_doigt.png",
     options: [
       {
         text: "Dire la vérité",
         action: "goToChapter('verite')",
       },
       {
-        text: "Vous faîtes à semblant de parler une autre langue",
+        text: "Essayer de parler une autre langue",
         action: "goToChapter('confusxxx')",
       },
       {
@@ -182,7 +182,7 @@ let chaptersObj = {
   confus: {
     subtitle: "Confus",
     text: "L'agresseur est confus et énervé et baisse sa garde sans s'en rendre compte, vous profitez de l'occasion pour y sauter dessus.",
-    img: "../assets/Images/confus.png",
+    img: "./assets/Images/confus.png",
     options: [
       {
         text: "À l'attaaaaaque!",
@@ -193,7 +193,7 @@ let chaptersObj = {
   confusxxx: {
     subtitle: "Confus",
     text: "L'agresseur est confus et énervé et baisse sa garde sans s'en rednre compte, vous profitez de l'occasion pour y sauter dessus.",
-    img: "../assets/Images/confus.png",
+    img: "./assets/Images/confus.png",
     options: [
       {
         text: "À l'attaaaaaque!",
@@ -204,7 +204,7 @@ let chaptersObj = {
   verite: {
     subtitle: "Vériter",
     text: "Vous avouez la vérité sur le couteau et il vous demande de vous lui donner.",
-    img: "../assets/Images/fusil_main.png",
+    img: "./assets/Images/fusil_main.png",
     options: [
       {
         text: "Lui remettre dans sa main",
@@ -220,18 +220,18 @@ let chaptersObj = {
   stab: {
     subtitle: "Mort - Poignardé",
     text: "Vous êtes parvenu à éloigné son arme à feu, mais il a récupérer votre couteau et il vous a poignardé.",
-    img: "../assets/Images/stab.jpg",
+    img: "./assets/Images/stab.jpg",
     options: [
       {
         text: "Recommencer",
-        action: "goToChapter('debut')",
+        action: "goToChapter('depart')",
       },
     ],
   },
   carJacking: {
     subtitle: "Car jacking",
     text: "Le cambrioleur vous menace de vous écartez du chemin afin qu'il accède à votre voiture.",
-    img: "../assets/Images/car_jacking.png",
+    img: "./assets/Images/car_jacking.png",
     options: [
       {
         text: "Le laisser passer",
@@ -243,10 +243,10 @@ let chaptersObj = {
       },
     ],
   },
-  carJackingXXX: {
+  carJackingxxx: {
     subtitle: "Car jacking",
     text: "Le cambrioleur vous menace de vous écartez du chemin afin qu'il accède à votre voiture.",
-    img: "../assets/Images/car_jacking.png",
+    img: "./assets/Images/car_jacking.png",
     options: [
       {
         text: "Vous laisser passer",
@@ -265,22 +265,22 @@ let chaptersObj = {
   homicide: {
     subtitle: "Homicide",
     text: "L'agresseur vous a vu venir et vous a tiré une balle dans la poitrine sans hésiter. Vous êtes mort.",
-    img: "../assets/Images/homicide.jpg",
+    img: "./assets/Images/homicide.jpg",
     options: [
       {
         text: "Recommencer",
-        action: "goToChapter('debut')",
+        action: "goToChapter('depart')",
       },
     ],
   },
   bye: {
     subtitle: "Bye bye",
     text: "Vous avez pris la bonne décision de laisser le voleur prendre votre voiture, parce que rien n'est plus important que votre propre vive. Vous êtes sain et sauf",
-    img: "../assets/Images/voiture_au_loin.jpg",
+    img: "./assets/Images/voiture_au_loin.jpg",
     options: [
       {
         text: "Recommencer",
-        action: "goToChapter('debut')",
+        action: "goToChapter('depart')",
       },
     ],
   },
@@ -296,21 +296,21 @@ function goToChapter(chapterName) {
   let texteHTML = document.querySelector(".texte");
   let imageHTML = document.getElementById("image");
 
-  console.log(chapitreTitre);
+  let button = document.querySelectorAll(".btn");
 
   chapitreHTML.innerHTML = chapitreTitre;
   texteHTML.innerHTML = chapitreTexte;
   imageHTML.src = chapitreImage;
 
-  let button = document.querySelectorAll(".btn");
-
   for (let index = 0; index <= 2; index++) {
     if (chapitreOptions[index] != undefined) {
       button[index].innerHTML = chapitreOptions[index].text;
       button[index].setAttribute("onclick", chapitreOptions[index].action);
-      button[index].classList.remove("vide");
+      button[index].classList.remove("none");
+      console.log(button[index]);
     } else {
-      button[index].classList.add("vide");
+      button[index].classList.add("none");
     }
   }
 }
+goToChapter("depart");
